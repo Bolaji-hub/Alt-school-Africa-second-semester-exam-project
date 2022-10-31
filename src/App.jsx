f29076b98b5a58cbba27af314af5d087a0f19736
Barakat Bolaji <barakatbolaji65@gmail.com>
2022-10-31 22:51:51 +0100

    pages for useReducer and useCounter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-
-const useCounter = (initialValue = 0) => {
-  const [count, setCount] = useState(initialValue)
-  const reset = () => setCount(initialValue)
-  const increment = () => setCount(count => count + 1)
-  const decrement = () => setCount(count => count - 1)
-  const setValue = (value) => setCount(value)
-
-  return {
-    count,
-    reset,
-    increment,
-    decrement,
-    setValue
-  }
-
-}
+import UseCounter from './UseCounter';
+import UseReducer from "./UseReducer";
 
 function App() {
-  const { count, increment, decrement, reset, setValue } = useCounter()
-
   return (
-    <div className="App">
-      <h1>{count}</h1>
-      <form onSubmit={event => {
-        event.preventDefault()
-        setValue(parseInt(event.target.elements[0].value))
-        }}>
-        <input type="number" />
-        <button>Set Value</button>
-      </form>
-      <div className="buttons">
-        <button onClick={increment}>
-          Increment
-        </button>
-        <button onClick={decrement}>
-          Decrement
-        </button>
-        <button onClick={reset}>
-          Reset
-        </button>
-        
-      </div>
-    </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<UseCounter />}></Route>
+      <Route path="/use-reducer" element={<UseReducer />}></Route>
+    </Routes>
+  </BrowserRouter>
   )
 }
 
---
+++ src/componenets/Counter.jsx
@@ -0,0 +1,28 @@
+export default function Counter(props) {
+  const { count, increment, decrement, reset, setValue } = props
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <form onSubmit={event => {
+        event.preventDefault()
+        setValue(parseInt(event.target.elements[0].value))
+        }}>
+        <input type="number" required />
+        <button>Set Value</button>
+      </form>
+      <div className="buttons">
+        <button onClick={increment}>
+          Increment
+        </button>
+        <button onClick={decrement}>
+          Decrement
+        </button>
+        <button onClick={reset}>
+          Reset
+        </button>
+        
+      </div>
+    </div>
+  )
+}
